35c46bc6de8cec840f9ff753bc0c717cb84ac34e
Lito Croy <146680987+litobot@users.noreply.github.com>
2024-05-11 15:59:30 -0600

    Show #initialize functional

--- lib/player.rb
+++ lib/player.rb
@@ -2,10 +2,11 @@ require './lib/card'
 require './lib/deck'
 
 class Player
-  attr_reader :name
-  
+  attr_reader :name, :deck
+
   def initialize(name, deck)
     @name = name
+    @deck = deck
   end
 
 end
