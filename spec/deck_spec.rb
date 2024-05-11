8c5cc85573be810315053ce70bcd5974c66b1a09
Lito Croy <146680987+litobot@users.noreply.github.com>
2024-05-10 18:36:46 -0600

    Shows #rank_of_card_at functional

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -21,20 +21,27 @@ RSpec.describe Deck do
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)  
     card3 = Card.new(:heart, 'Ace', 14)
-
+    
+    cards = [card1, card2, card3]
+    
+    deck = Deck.new(cards)
+    
+    expect(deck.cards).to eq([card1, card2, card3])
+  end
+  
+  it "returns card rank via array indices" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)  
+    card3 = Card.new(:heart, 'Ace', 14)
+    
     cards = [card1, card2, card3]
     
     deck = Deck.new(cards)
 
-    expect(deck.cards).to eq([card1, card2, card3])
+    expect(deck.rank_of_card_at(0)).to eq(12)
+    expect(deck.rank_of_card_at(2)).to eq(14)
   end
 end
-
-  # pry(main)> deck = Deck.new(cards)
-  # #=> #<Deck:0x007fbfd2984808 @cards=[#<Card:0x007fbfd18490e8...>, #<Card:0x007fbfd19f4fa0...>, #<Card:0x007fbfd18555a0...>]>
-
-  # pry(main)> deck.cards
-  # #=> [#<Card:0x007fbfd18490e8...>, #<Card:0x007fbfd19f4fa0...>, #<Card:0x007fbfd18555a0...>]
 
   # pry(main)> deck.rank_of_card_at(0)
   # #=> 12
