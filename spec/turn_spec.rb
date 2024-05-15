fe42e9a22653fefe13ea3c43f17b58f51bc5159c
Lito Croy <146680987+litobot@users.noreply.github.com>
2024-05-15 16:25:23 -0600

    Refactor :mutually_assured_destruction type with before(:each) do

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -118,71 +118,39 @@ RSpec.describe Turn do
 
   describe ":mutually_assured_destruction type" do
 
+      before(:each) do
+        @card1 = Card.new(:heart, 'Jack', 11)
+        @card2 = Card.new(:heart, '10', 10)    
+        @card3 = Card.new(:heart, '9', 9)    
+        @card4 = Card.new(:diamond, 'Jack', 11)    
+        @card5 = Card.new(:heart, '8', 8)    
+        @card6 = Card.new(:diamond, '8', 8)    
+        @card7 = Card.new(:heart, '3', 3)    
+        @card8 = Card.new(:diamond, '2', 2)    
+
+        @deck1 = Deck.new([@card1, @card2, @card5, @card8])
+        @deck2 = Deck.new([@card4, @card3, @card6, @card7])
+    
+        @player1 = Player.new("Megan", @deck1)
+        @player2 = Player.new("Aurora", @deck2)
+    
+        @turn = Turn.new(@player1, @player2)
+      end
+
     it "has a turn type" do
-      card1 = Card.new(:heart, 'Jack', 11)
-      card2 = Card.new(:heart, '10', 10)    
-      card3 = Card.new(:heart, '9', 9)    
-      card4 = Card.new(:diamond, 'Jack', 11)    
-      card5 = Card.new(:heart, '8', 8)    
-      card6 = Card.new(:diamond, '8', 8)    
-      card7 = Card.new(:heart, '3', 3)    
-      card8 = Card.new(:diamond, '2', 2)    
-  
-      deck1 = Deck.new([card1, card2, card5, card8])
-      deck2 = Deck.new([card4, card3, card6, card7])
-  
-      player1 = Player.new("Megan", deck1)
-      player2 = Player.new("Aurora", deck2)
-  
-      turn = Turn.new(player1, player2) 
-      
-      expect(turn.type).to eq(:mutually_assured_destruction)
+      expect(@turn.type).to eq(:mutually_assured_destruction)
     end
     
     it "does not have a winner of " do
-      card1 = Card.new(:heart, 'Jack', 11)
-      card2 = Card.new(:heart, '10', 10)    
-      card3 = Card.new(:heart, '9', 9)    
-      card4 = Card.new(:diamond, 'Jack', 11)    
-      card5 = Card.new(:heart, '8', 8)    
-      card6 = Card.new(:diamond, '8', 8)    
-      card7 = Card.new(:heart, '3', 3)    
-      card8 = Card.new(:diamond, '2', 2)
-  
-      deck1 = Deck.new([card1, card2, card5, card8])
-      deck2 = Deck.new([card4, card3, card6, card7])
-  
-      player1 = Player.new("Megan", deck1)
-      player2 = Player.new("Aurora", deck2)
-  
-      turn = Turn.new(player1, player2) 
-      
-      expect(turn.winner).to eq("No Winner")
+      expect(@turn.winner).to eq("No Winner")
     end
     
     it "removes 3 cards of each player's deck from play" do
-      card1 = Card.new(:heart, 'Jack', 11)
-      card2 = Card.new(:heart, '10', 10)    
-      card3 = Card.new(:heart, '9', 9)    
-      card4 = Card.new(:diamond, 'Jack', 11)    
-      card5 = Card.new(:heart, '8', 8)    
-      card6 = Card.new(:diamond, '8', 8)    
-      card7 = Card.new(:heart, '3', 3)    
-      card8 = Card.new(:diamond, '2', 2)
-
-      deck1 = Deck.new([card1, card2, card5, card8])
-      deck2 = Deck.new([card4, card3, card6, card7])
-  
-      player1 = Player.new("Megan", deck1)
-      player2 = Player.new("Aurora", deck2)
-  
-      turn = Turn.new(player1, player2) 
+      @winner = @turn.winner
+      @turn.pile_cards
       
-      winner = turn.winner
-      turn.pile_cards
-      
-      expect(player1.deck.cards).to eq([card8])
-      expect(player2.deck.cards).to eq([card7])
+      expect(@player1.deck.cards).to eq([@card8])
+      expect(@player2.deck.cards).to eq([@card7])
     end
   end
 end
